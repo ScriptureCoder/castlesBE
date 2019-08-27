532c974036184e17190990253d8e2a8b4e77ab93
ScriptureCoder <akinfenwasamson@gmail.com>
2019-08-27 21:32:19 +0100

    update

--- dem.sh
+++ dem.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-php artisan migrate:refresh --seed
-php artisan passport:install
+#php artisan migrate:refresh --seed
+#php artisan passport:install
 
+git add .
+git commit -m "update"
+git push
+git push origin2
